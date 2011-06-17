bd77ec774627ebf6d94279b26c6bad8a64f6b7b4
Eric Smith <eric@8thlight.com>
2011-06-17 07:07:53 -0500

    Initial Commit

---
+++ HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by Eric Smith on 6/17/11.
+//  Copyright 2011 8th Light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
